e5e6d73ada966a643364201eac58c2e8f97a9bb7
Zeigfreid <various.authors@gmail.com>
2014-06-20 09:14:43 -0700

    Added the Neighbourhood table
    It is a bunch of functions that need to be moved into their own file.
    I could replace it with data, but I'm not sure that would be better...
    We will see. Too many states to represent it elegantly as data.
    
    Also added water, which erodes walls, and pools which have a chance of
    sticking around.

--- cellular_automata.lua
+++ cellular_automata.lua
@@ -1,7 +1,121 @@
+EMPTY    = 0
+SOLID    = 1
+MONSTER  = 2
+TREASURE = 3
+WATER    = 4
+POOL     = 5
+
+-- returns a function that returns the given value
+local identity = function (value)
+    return function (...)
+        return value
+    end
+end
+
 return (function ()
     local component = "CellularAutomata"
     local needs = { "Cells" }
-    local threshold = 4
+
+    local maps = {}
+
+    maps[EMPTY] = function (neighbourhood)
+        local solid      = neighbourhood[SOLID]
+        local neighbours = solid + neighbourhood[MONSTER]
+        local water      = neighbourhood[WATER]
+        local threshold  = 4
+        local result = EMPTY
+
+        if solid > threshold then
+            result = 1
+        elseif solid < threshold then
+            result = 0
+        end
+
+        -- if 9 squares are empty, magic happens!
+        if neighbours == 0 then
+            local random = rng:random()
+            -- a small percent of the time, features will appear
+            if random < 0.001 then
+                result = TREASURE
+            elseif random < 0.01 then
+                result = MONSTER
+            end
+        end
+
+        if water > 0 then
+            local random = rng:random()
+            -- a small percent of the time, features will appear
+            if water == 1 and random < 0.2 then
+                result = WATER
+            else
+                result = POOL
+            end
+        end
+
+        return result
+    end
+
+    maps[SOLID] = function (neighbourhood)
+        local solid     = neighbourhood[SOLID]
+        local water     = neighbourhood[WATER] + neighbourhood[POOL]
+        local threshold = 4
+        local result = SOLID
+
+        if solid == 8 then
+            local random = rng:random()
+            -- a small percent of the time, features will appear
+            if random < 0.001 then
+                result = WATER
+            end
+        elseif solid > threshold then
+            result = SOLID
+        elseif solid < threshold then
+            result = EMPTY
+        end
+
+        if water > 0 then
+            result = EMPTY
+        end
+
+        return result
+    end
+
+    maps[MONSTER]  = identity(MONSTER)
+    maps[TREASURE] = identity(TREASURE)
+    maps[POOL]     = function (neighbourhood)
+        local pool = neighbourhood[POOL]
+        local result = POOL
+
+        if pool == 0 or pool == 1 or pool == 3 then
+            result = EMPTY
+        end
+
+        return result
+    end
+    maps[WATER]    = function (neighbourhood)
+        local result = WATER
+
+        local random = rng:random()
+        -- a small percent of the time, features will appear
+        if random < 0.7 then
+            result = POOL
+        end
+
+        return result
+    end
+
+    local states = {
+        { -- empty
+            { -- solid
+            },
+        },
+        { -- solid
+
+        },
+        { -- treasure
+
+        }
+    }
 
     local update_automata = function (key, entity)
         local cells     = entity["Cells"]
@@ -15,11 +129,10 @@ return (function ()
         for i = 1, #cells do
             _next[i] = {}
 
-            inspect({ width, height })
-
             for j = 1, #cells do
                 _next[i][j] = cells[i][j]
-                local neighbours = 0
+                local neighbourhood = { 0, 0, 0, 0, 0 }
+                neighbourhood[EMPTY] = 0
 
                 -- visit its neighbours (it has 8 neighbours in 2 space)
                 for k = 0, 8 do
@@ -28,34 +141,20 @@ return (function ()
 
                     if cells[row] ~= nil and cells[row][col] ~= nil then
                         if row ~= i or col ~= j then
-                            if cells[row][col] == 1 then
-                                neighbours = neighbours + 1
-                            elseif cells[row][col] == 2 then
-                                neighbours = neighbours - 1
-                            end
+                            neighbourhood[cells[row][col]] = neighbourhood[cells[row][col]] + 1
                         end
                     end
 
+                    -- count out of bounds as solid
                     if row < 1 or row > height or col < 1 or col > width then
                         if row ~= i or col ~= j then
-                            neighbours = neighbours + 1
+                            neighbourhood[SOLID] = neighbourhood[SOLID] + 1
                         end
                     end
                 end
 
-                if neighbours > threshold then
-                    _next[i][j] = 1
-                elseif neighbours == 0 then
-                    local random = rng:random()
-                    -- a small percent of the time, features will appear
-                    if random < 0.001 then
-                        _next[i][j] = 3
-                    elseif random < 0.01 then
-                        _next[i][j] = 2
-                    end
-
-                elseif neighbours < threshold then
-                    _next[i][j] = 0
+                if cells[i][j] then
+                    _next[i][j] = maps[cells[i][j]](neighbourhood)
                 end
             end
         end
--- main.lua
+++ main.lua
@@ -20,8 +20,11 @@ local state_machine
 global.tile_size = 8
 rng = love.math.newRandomGenerator(os.time())
 
+local start_x = 100
+local start_y = 75
+
 local Cells = {
-    cells = Matrix(100, 75, Generators.random(0, 1))
+    cells = Matrix(start_x, start_y, Generators.random(0, 1))
 }
 
 Entities = {
@@ -48,8 +51,8 @@ Entities = {
             world = Cells.cells
         },
         Positioned = {
-            x = 50, y = 50,
-            old_x = 50, old_y = 50
+            x = start_x - 1, y = start_y - 1,
+            old_x = start_x - 1, old_y = start_y - 1
         }
     },
     {
@@ -57,18 +60,22 @@ Entities = {
         Drawable = {
             background     = { 0, 0, 0 },
             cell_size      = global.tile_size,
-            cell_color     = { 50, 200, 50 },
-            feature_color  = { 200, 50, 50 },
-            treasure_color = { 200, 200, 50 },
+            color          = {
+                { 50, 200, 50 },  -- SOLID
+                { 200, 50, 50 },  -- MONSTER
+                { 200, 200, 50 }, -- TREASURE
+                { 50, 50, 200},   -- WATER
+                { 50, 150, 200},   -- POOL
+            },
             needs          = { "Cells" },
             draw       = function (self, cells)
                 local r, g, b       = love.graphics.getColor()
                 local cell_size     = self.cell_size
                 local background    = self.background
-                local cell_color    = self.cell_color
-                local feature_color = self.feature_color
-                local treasure_color = self.treasure_color
+                local color         = self.color
                 local length        = #cells * cell_size
+                local grid_h        = #cells
+                local grid_w        = #(cells[1])
 
                 local ftx, fty = 0, 0
                 love.graphics.push()
@@ -76,20 +83,14 @@ Entities = {
                 love.graphics.translate(ftx, fty)
 
                 love.graphics.setColor(background)
-                for i = 0, #cells do
+                for i = 0, grid_h do
                     love.graphics.line(i*cell_size, 0, i*cell_size, length)
                     love.graphics.line(0, i*cell_size, length, i*cell_size)
 
                     if i > 0 then
-                        for j = 1, #cells do
-                            if cells[i][j] == 1 then
-                                love.graphics.setColor(cell_color)
-                                love.graphics.rectangle("fill", (i - 1)*cell_size + 1, (j - 1)*cell_size + 1, cell_size - 2, cell_size - 2)
-                            elseif cells[i][j] == 2 then
-                                love.graphics.setColor(feature_color)
-                                love.graphics.rectangle("fill", (i - 1)*cell_size + 1, (j - 1)*cell_size + 1, cell_size - 2, cell_size - 2)
-                            elseif cells[i][j] == 3 then
-                                love.graphics.setColor(treasure_color)
+                        for j = 1, grid_w do
+                            if cells[i][j] ~= EMPTY then
+                                love.graphics.setColor(color[cells[i][j]])
                                 love.graphics.rectangle("fill", (i - 1)*cell_size + 1, (j - 1)*cell_size + 1, cell_size - 2, cell_size - 2)
                             end
                         end
